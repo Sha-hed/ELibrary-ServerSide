09fb71d08bedd4ad940530b3919f16436f17c014
Sha-hed <shahedcse14@gmail.com>
2024-05-10 14:02:42 +0600

    mongodb setup completed

--- index.js
+++ index.js
@@ -1,17 +1,37 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-require('dotenv').config()
-const port = process.env.PORT || 5000
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
 
+app.use(cors());
+app.use(express.json());
 
-app.use(cors())
-app.use(express.json())
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ojv3wo1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+console.log(uri);
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
 
-app.get('/', async(req,res)=>{
-    res.send('We are Heading Towards completing Assignment-11')
-})
+async function run() {
+  try {
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+  }
+}
+run().catch(console.dir);
 
-app.listen(port, ()=>{
-    console.log(`Our backend Server is running for assignment on Port : ${port}`)
-})
+app.get("/", async (req, res) => {
+  res.send("We are Heading Towards completing Assignment-11");
+});
+
+app.listen(port, () => {
+  console.log(`Our backend Server is running for assignment on Port : ${port}`);
+});
