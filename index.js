10067750a3cdac005b7bca72e309ed3f303df378
Sha-hed <shahedcse14@gmail.com>
2024-05-11 20:10:14 +0600

    Fetch data by specific id

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
@@ -38,6 +38,13 @@ async function run() {
       const result = await BookCollection.find(query).toArray();
       res.send(result);
     });
+
+    app.get('/singleBook/:id', async(req,res)=>{
+      const id = req.params.id;
+      const query = { _id : new ObjectId(id)};
+      const result = await BookCollection.findOne(query);
+      res.send(result);
+    })
 
     app.post("/add", async (req, res) => {
       const book = req.body;
