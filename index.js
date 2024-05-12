6a9ecc689a3ee9aff21ba3331faa6bccb7a32647
Sha-hed <shahedcse14@gmail.com>
2024-05-12 23:39:47 +0600

    Book Quantity Update complete

--- index.js
+++ index.js
@@ -21,6 +21,10 @@ async function run() {
   try {
     const BookCategory = client.db("Assignment-11").collection("Category");
     const BookCollection = client.db("Assignment-11").collection("AllBooks");
+    const BorrowedCollection = client.db("Assignment-11").collection("Borrowed");
+
+
+    //Get Book or Category  Section 
 
     app.get("/cat", async (req, res) => {
       const result = await BookCategory.find({}).toArray();
@@ -39,18 +43,80 @@ async function run() {
       res.send(result);
     });
 
-    app.get('/singleBook/:id', async(req,res)=>{
+    app.get("/singleBook/:id", async (req, res) => {
       const id = req.params.id;
-      const query = { _id : new ObjectId(id)};
+      const query = { _id: new ObjectId(id) };
       const result = await BookCollection.findOne(query);
       res.send(result);
+    });
+
+
+    app.get('/borrowedBooks', async(req,res)=>{
+      const email = req.query.email;
+      const filter = { bor_email : email}
+      const result = await BorrowedCollection.find(filter).toArray();
+      res.send(result);
     })
+    app.get('/boiNai', async(req,res)=>{
+      const email = req.query.email;
+      const filter = { bor_email : email}
+      const result = await BorrowedCollection.find(filter).toArray();
+      res.send(result);
+    })
+
+    app.get('/please/:email', async(req,res)=>{
+      const email = req.params.email;
+      const filter = { bor_email : email}
+      const result = await BorrowedCollection.find(filter).toArray();
+      res.send(result);
+
+    })
+
+    //Post Book Section 
 
     app.post("/add", async (req, res) => {
       const book = req.body;
       const result = await BookCollection.insertOne(book);
       res.send(result);
     });
+
+    app.post("/bor", async (req, res) => {
+      const bor_book = req.body;
+      const result = await BorrowedCollection.insertOne(bor_book);
+      res.send(result);
+    });
+
+    //Update Book Section
+
+    app.patch("/dec", async (req, res) => {
+      const book = req.body;
+      const filter = { _id: new ObjectId(book._id) };
+      const updateDoc = {
+        $inc: { quantity: -1 },
+        // $inc : { quantity : 1 }
+      };
+      const result = await BookCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
+    app.patch("/returnQuantity/:id", async(req,res)=>{
+      const id = req.params.id;
+      const filter = { _id : new ObjectId(id)};
+      const updateDoc = {
+        $inc : { quantity : 1 },
+      }
+      const result = await BookCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    })
+
+    //Delete Book Section : 
+
+    // app.delete("/returnBook/:id", async(req,res)=>{
+    //   const id = req.params.id;
+    //   const filter = { _id : new ObjectId(id)};
+    //   const result = await BorrowedCollection.deleteOne(filter);
+    //   res.send(result);
+    // })
 
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
