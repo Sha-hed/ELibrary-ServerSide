fab825d7d75e0bad635c5bd5339c4ac1bcce9ca3
Sha-hed <shahedcse14@gmail.com>
2024-05-13 22:03:37 +0600

    Verify Tokan added

--- index.js
+++ index.js
@@ -2,12 +2,37 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser')
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:5173'],
+  credentials: true
+}));
 app.use(express.json());
+app.use(cookieParser())
+
+const options = {
+  httpOnly: true,
+  secure: process.env.NODE_ENV === "production",
+  sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+};
+
+const verifyToken = (req,res,next)=>{
+  const token = req?.cookies?.token
+  if(!token){
+    return res.status(401).send({message: 'unauthorized Access'})
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded)=>{
+    if(err){
+      return res.status().send({message: 'Unauthorized Access'})
+    }
+    req.user = decoded;
+    next();
+  } )
+}
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ojv3wo1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 const client = new MongoClient(uri, {
@@ -26,6 +51,21 @@ async function run() {
       .db("Assignment-11")
       .collection("Borrowed");
 
+      // JWT Section
+
+      app.post('/jwt', async(req,res)=>{
+        const user = req.body;
+        const token = jwt.sign( user,process.env.ACCESS_TOKEN, {expiresIn: '1h'});
+        res
+        .cookie('token', token,options)
+        .send({success: true});
+
+      })
+
+      app.post('/logout', async(req,res)=>{
+        res.clearCookie('token',{maxAge:0,})
+      })
+
     //Get Book or Category  Section
 
     app.get("/cat", async (req, res) => {
@@ -33,7 +73,7 @@ async function run() {
       res.send(result);
     });
 
-    app.get("/books", async (req, res) => {
+    app.get("/books", verifyToken, async (req, res) => { 
       const result = await BookCollection.find().toArray();
       res.send(result);
     });
@@ -88,7 +128,7 @@ async function run() {
 
     //Post Book Section
 
-    app.post("/add", async (req, res) => {
+    app.post("/add", verifyToken, async (req, res) => {
       const book = req.body;
       const result = await BookCollection.insertOne(book);
       res.send(result);
