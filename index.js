f670bb565571539502e43e0c85074739846d1667
Sha-hed <shahedcse14@gmail.com>
2024-06-13 18:14:59 +0600

    Change Route

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const jwt = require('jsonwebtoken');
-const cookieParser = require('cookie-parser')
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
-app.use(cors({
-  origin: ['http://localhost:5173'],
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5174",
+      "http://localhost:5173",
+      "https://assignment-11-5ffa8.web.app",
+      "https://assignment-11-5ffa8.firebaseapp.com",
+    ],
+    credentials: true,
+  })
+);
 app.use(express.json());
-app.use(cookieParser())
+app.use(cookieParser());
 
 const options = {
   httpOnly: true,
@@ -20,19 +27,19 @@ const options = {
   sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
 };
 
-const verifyToken = (req,res,next)=>{
-  const token = req?.cookies?.token
-  if(!token){
-    return res.status(401).send({message: 'unauthorized Access'})
+const verifyToken = (req, res, next) => {
+  const token = req?.cookies?.token;
+  if (!token) {
+    return res.status(401).send({ message: "unauthorized Access" });
   }
-  jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded)=>{
-    if(err){
-      return res.status().send({message: 'Unauthorized Access'})
+  jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
+    if (err) {
+      return res.status().send({ message: "Unauthorized Access" });
     }
     req.user = decoded;
     next();
-  } )
-}
+  });
+};
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ojv3wo1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 const client = new MongoClient(uri, {
@@ -51,21 +58,15 @@ async function run() {
       .db("Assignment-11")
       .collection("Borrowed");
 
-      // JWT Section
+    // JWT Section
 
-      app.post('/jwt', async(req,res)=>{
-        const user = req.body;
-        const token = jwt.sign( user,process.env.ACCESS_TOKEN, {expiresIn: '1h'});
-        res
-        .cookie('token', token,options)
-        .send({success: true});
-
-      })
-
-      app.post('/logout', async(req,res)=>{
-        res.clearCookie('token',{maxAge:0,})
-      })
-
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN, {
+        expiresIn: "1h",
+      });
+      res.cookie("token", token, options).send({ success: true });
+    });
     //Get Book or Category  Section
 
     app.get("/cat", async (req, res) => {
@@ -73,7 +74,7 @@ async function run() {
       res.send(result);
     });
 
-    app.get("/books", verifyToken, async (req, res) => { 
+    app.get("/books", async (req, res) => {
       const result = await BookCollection.find().toArray();
       res.send(result);
     });
@@ -163,19 +164,19 @@ async function run() {
       res.send(result);
     });
 
-    app.patch('/UpdateBooks/:id', async(req,res)=>{
+    app.patch("/UpdateBooks/:id", async (req, res) => {
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) };
       const bookInfo = req.body;
       console.log(bookInfo);
       const updateDoc = {
-        $set : {
-         ...bookInfo,
-        }
+        $set: {
+          ...bookInfo,
+        },
       };
       const result = await BookCollection.updateOne(filter, updateDoc);
       res.send(result);
-    })
+    });
 
     // Delete Book Section :
 
